d8961b6848c19b35fcc8cdf96ead7322cb3d5890
Leninux <redrudeboy@gmail.com>
2014-11-28 20:48:32 +0100

    Borrat Bootstrap, afegit jcarousel.

--- head.php
+++ head.php
@@ -10,12 +10,21 @@
 <link type="text/css" rel="stylesheet" href="css/MC.css"/>
 
 <script src="js/jquery-1.9.1.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!--
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+-->
+
+<script src="js/jquery.jcarousel.min.js"></script>
+
+<!--
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="./css/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="./css/bootstrap/bootstrap-theme.css">
+-->
+
 <!-- Icons -->
 <title><?php echo (empty($page_title)) ? 'MC CONSULTA AUDITIVA' : $page_title; ?></title>
 
