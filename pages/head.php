1214d4873d866194c35fb4d82abab2a417601351
Leninux <redrudeboy@gmail.com>
2014-12-12 18:34:19 +0100

    Improved SEO

--- pages/head.php
+++ pages/head.php
@@ -5,36 +5,33 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 <meta name="description" content="MC CONSULTA AUDITIVA">
-<meta name="description" lang="cat" content="" />
-<meta name="description" lang="es" content="" />
-<meta name="description" lang="en" content="" />
-<meta name="description" lang="it" content="" />
+<meta name="description" lang="es" content="Somos profesionales titulados en audiologia con experiencia en el sector de m&acute;s 30 a&ntilde;os. Efectuamos estudios audiol&oacute;gicos completo y aplicaciones de aud&iacute;fonos digitales personalizados sistema MC" />
+<meta name="description" lang="it" content="Siamo specialisti titolati in tecniche protesiche con piu'di 30 anni di esperienza nel settore. Realiziamo esame audiologico completo e applicazioni di apparecchi acustici digitali personalizzati sistema MC" />
+<meta name="description" lang="cat" content="Som un equip d&#39;especialistes titulats en audiol&ograve;gia amb experi&egrave;ncia en el sector des de fa m&eacute;s de 30 anys." />
+<meta name="description" lang="en" content="We are a team of specialists graduated in Audiology with over 30 years of experience." />
 
-<meta name="keywords" content="MC CONSULTA AUDITIVA, aud&iacute;fonos,aud&iacute;fonos digitales, aud">
+<meta name="keywords" content="MC CONSULTA AUDITIVA, MCCONSULTAAUDITIVA, aud&iacute;fonos digitales, gama alta, exclusivos, barcelona, mandri, cl&iacute;nica del o&iacute;do, pr&oacute;tesis auditiva digitales, apparecchi acustici digitali, clínica auditiva, apparecchi acustici gamma alta">
 <meta name="author" content="MC CONSULTA AUDITIVA">
 
 <link rel="apple-touch-icon" href="apple-touch-icon.png">
 
 <link type="text/css" rel="stylesheet" href="css/normalize.css"/>
-<link type="text/css" rel="stylesheet" href="css/MC.css"/>
-
-<script src="js/jquery-1.9.1.min.js"></script>
-<script src="js/main.js"></script>
-<!--
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
--->
-
-<script src="js/jquery.jcarousel.min.js"></script>
-
-<!--
+<?php /*
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
 <link rel="stylesheet" href="./css/bootstrap/bootstrap.min.css">
 <link rel="stylesheet" href="./css/bootstrap/bootstrap-theme.css">
--->
+*/ ?>
+<link type="text/css" rel="stylesheet" href="css/MC.css"/>
+
+<script src="js/jquery-1.9.1.min.js"></script>
+<script src="js/main.js"></script>
+<?php /*
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+*/ ?>
+<script src="js/jquery.jcarousel.min.js"></script>
 
 <!-- Icons -->
 <title><?php echo (empty($page_title)) ? 'MC CONSULTA AUDITIVA' : $page_title; ?></title>
-
